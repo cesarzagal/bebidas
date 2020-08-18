08f31a46d9f57e560d2b6672b69dc8bd03604979
Cesar <cesarzagal@gmail.com>
2020-08-18 18:14:00 -0400

    Aplicacion para la busqueda de mezclas de Bebidas con ReactJs

---
+++ src/context/ModalContext.js
@@ -0,0 +1,33 @@
+import React, {createContext, useEffect, useState} from 'react';
+import axios from 'axios';
+
+export const ModalContext = createContext();
+
+const ModalProvider = (props) => {
+    const [idReceta, guardarIdReceta] = useState(null);
+    const [informacion, guardarReceta] = useState({});
+
+    //una vez que tenemos la receta, llamamos a la api
+    useEffect(() => {
+        const obtenerReceta = async () => {
+            if(!idReceta) return;
+            const url=`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idReceta}`;
+            const resultado = await axios.get(url);
+            guardarReceta(resultado.data.drinks[0]);
+            
+        }
+        obtenerReceta();
+    }, [idReceta])
+    return (
+        <ModalContext.Provider
+            value={{
+                informacion,
+                guardarIdReceta,
+                guardarReceta
+            }}
+        >
+            {props.children}
+        </ModalContext.Provider>
+    )
+}
+export default ModalProvider;
